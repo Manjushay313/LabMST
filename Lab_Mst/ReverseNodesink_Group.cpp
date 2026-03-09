ad3c4183a8502f875cab7d175a2e5a854fb85adf
Manjushay313 <manjushay045@gmail.com>
2026-03-09 15:30:08 +0530

    Add files via upload

---
+++ Lab_Mst/ReverseNodesink_Group.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* node = head;
+        for (int i = 0; i < k; i++) {
+            if (!node) return head;
+            node = node->next;
+        }
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        for (int i = 0; i < k; i++) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head->next = reverseKGroup(curr, k);
+        return prev;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val;
+        if (head->next) cout << " -> ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+ListNode* createList(int arr[], int n) {
+    if (n == 0) return nullptr;
+    ListNode* head = new ListNode(arr[0]);
+    ListNode* curr = head;
+    for (int i = 1; i < n; i++) {
+        curr->next = new ListNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    ListNode* head = createList(arr, n);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    int k = 2;
+    head = sol.reverseKGroup(head, k);
+
+    cout << "Reversed in groups of " << k << ": ";
+    printList(head);
+
+    return 0;
+}
